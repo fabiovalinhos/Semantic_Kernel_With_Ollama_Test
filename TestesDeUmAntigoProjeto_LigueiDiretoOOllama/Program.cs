f3e1a63c698a722724c1f0bc326931cff0424d47
fabiovalinhos <fabiovalinhos@gmail.com>
2025-07-17 14:22:40 -0300

    Ajustes no launch

--- TestesDeUmAntigoProjeto_LigueiDiretoOOllama/Program.cs
+++ TestesDeUmAntigoProjeto_LigueiDiretoOOllama/Program.cs
@@ -1,12 +1,12 @@
-﻿var ia = new IAService();
+﻿// var ia = new IAService();
 
-while (true)
-{
-    Console.WriteLine("Digite sua pergunta:");
-    var pergunta = Console.ReadLine();
+// while (true)
+// {
+//     Console.WriteLine("Digite sua pergunta:");
+//     var pergunta = Console.ReadLine();
 
-    var resposta = await ia.PerguntarComTodosAsync(pergunta);
+//     var resposta = await ia.PerguntarComTodosAsync(pergunta);
 
-    Console.WriteLine("\n💬 Resposta final:");
-    Console.WriteLine(resposta);
-}
+//     Console.WriteLine("\n💬 Resposta final:");
+//     Console.WriteLine(resposta);
+// }
